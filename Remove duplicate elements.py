e2c721b8a5900efcdc2861d24335688803312eee
vsuravaram <vr.0451@gmail.com>
2022-12-25 22:12:01 -0600

    update

--- Remove duplicate elements.py
+++ Remove duplicate elements.py
@@ -2,9 +2,14 @@
 
 l=[10,1,2,3,4,1,2,3,4]
 l_uniq=[]
+count=0
 for ele in range(len(l)):
 
     if l[ele] not in l_uniq:
         l_uniq.append(l[ele])
+    elif l[ele] in l_uniq:
+        count=count+1
+
 print("Actual list elements: ",l)
 print("Unique elements in the list: ",l_uniq)
+print("Duplicate elements count: ",count )
